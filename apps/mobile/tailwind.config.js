dfed08237ee2d8fa7836837fcd1d8621c3e5fb9c
jonheaven <jheaven@gmail.com>
2026-05-08 19:03:20 -0700

    updates

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,6 +1,8 @@
-const uiPreset = require('../../packages/ui/tailwind.config');
+const uiConfig = require('../../packages/ui/tailwind.config');
+const nativewindPreset = require('nativewind/preset');
 
 module.exports = {
-  presets: [uiPreset],
+  ...uiConfig,
+  presets: [nativewindPreset],
   content: ['./App.{js,jsx,ts,tsx}', './index.{js,jsx,ts,tsx}', './**/*.{js,jsx,ts,tsx}', '../../packages/ui/src/**/*.{js,ts,jsx,tsx}']
 };
